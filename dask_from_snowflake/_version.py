a66ab1aae858994e17ecab6ae0c0c6dda9325adf
Greg Hayes <hayesgb@gmail.com>
2021-05-10 21:54:42 -0500

    Fix folder

--- dask_from_snowflake/_version.py
+++ dask_from_snowflake/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "dask_from_snowflake-"
-    cfg.versionfile_source = "read_snowflake/_version.py"
+    cfg.versionfile_source = "dask_from_snowflake/_version.py"
     cfg.verbose = False
     return cfg
 
